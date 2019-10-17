44d0e33926d789a0f2ae05c96efed6d782f42001
Kiki Lin <mikey2862@gmail.com>
2019-10-17 08:39:33 +0800

    get temperature from file

--- highs_lows.py
+++ highs_lows.py
@@ -4,10 +4,16 @@
 """
 import csv
 
+#Get high temperature form file
 filename = 'sitka_weather_2018_full.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    #for index, column_header in enumerate(header_row):
+        #print(index, column_header)
+    highs = []
+    for row in reader:
+        highs.append(row[8])
+
+    print(highs)
